f434fdb047c2a3d7118e92d633be8fe5c34d19d8
Tomasz Knapik <hi@tmkn.org>
2019-04-11 21:09:19 +0100

    Restore the Wagtail root URL
    Otherwise the admin won't work (it fails on reverse for the "LIVE"
    links).

--- bakerydemo/urls.py
+++ bakerydemo/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
@@ -10,6 +11,7 @@ urlpatterns = [
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
+    url(r'', include(wagtail_urls)),
 ]
 
 
